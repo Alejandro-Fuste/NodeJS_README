ecab69098d9dd7a7e3a4acbbfe10010bed0a5e82
Alejandro F <afuste7@gmail.com>
2021-02-03 09:13:22 -0500

    added writeFile function to return a Promise

--- develop/utils/writeToFile.js
+++ develop/utils/writeToFile.js
@@ -1,11 +1,17 @@
 const fs = require('fs');
 
-function writeToFile(fileName, data) {
-	fs.writeFile(fileName, data, (err) => {
-		if (err) {
-			throw err;
-		}
+const writeFile = (fileContent) => {
+	return new Promise((resolve, reject) => {
+		fs.writeFile('README.md', fileContent, (err) => {});
 	});
-}
+};
+
+// function writeToFile(fileName, data) {
+// 	fs.writeFile(fileName, data, (err) => {
+// 		if (err) {
+// 			throw err;
+// 		}
+// 	});
+// }
 
 module.exports = writeToFile;
