c2a69cc50fd68249c853e818cdab52f4ac849362
Alejandro F <afuste7@gmail.com>
2021-02-03 09:17:30 -0500

    added resolve to writeFile and exported it

--- develop/utils/writeToFile.js
+++ develop/utils/writeToFile.js
@@ -2,7 +2,15 @@ const fs = require('fs');
 
 const writeFile = (fileContent) => {
 	return new Promise((resolve, reject) => {
-		fs.writeFile('README.md', fileContent, (err) => {});
+		fs.writeFile('README.md', fileContent, (err) => {
+			if (err) {
+				reject(err);
+			}
+			resolve({
+				ok: true,
+				message: 'File Created!'
+			});
+		});
 	});
 };
 
@@ -14,4 +22,4 @@ const writeFile = (fileContent) => {
 // 	});
 // }
 
-module.exports = writeToFile;
+module.exports = writeFile;
